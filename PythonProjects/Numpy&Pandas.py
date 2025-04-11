1e24e6ea65b38808bf02c7b6eb0bdf6ab52bcb4d
moh <KingStars202@gmail.com>
2025-04-11 16:28:11 +0300

    ..

--- PythonProjects/Numpy&Pandas.py
+++ PythonProjects/Numpy&Pandas.py
@@ -1,5 +1,30 @@
-import pandas as pd 
-data = pd.Series((0.25, 0.5, 0.75, 1.0)) 
-print(data[1]) 
-print(data[1:3]) 
-print(data[1:3:2])
+
+import pandas as pd
+import matplotlib as plt
+
+#الطلب الأول
+
+dic={'math':[50,45,80,65],'prog':[51,45,72,84],'electric':[46,91,73,42]}
+std= ["ahmed","ali","maha","sara"]
+df= pd.DataFrame(dic,index=std)
+
+df.loc['rabee']=[30,20,40]
+
+df["max"]=df.max(axis=1)
+df["min"]=df.min(axis=1)
+df["ave"]=df[['math','prog','electric']].mean(axis=1)
+
+def update_mark(df,name,s,d):
+    df.loc[name,s]=d
+    return df
+df=update_mark(df,'ahmed','math',100)
+df['result']=df['ave'].apply(lambda x: 'pass' if x>50 else 'fail')
+df['new prog']=df['prog'].apply(lambda x: x+10)
+
+df.plot(kind='bar', x=['math','prog'], y=['Value1', 'Value2'], edgecolor='black')
+
+plt.show()
+
+print(df)
+df1=df.query('ave>50')
+print(df1)
